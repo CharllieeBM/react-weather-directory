0583f22eef79e70b11d38b658c92d601d438d66e
CharllieeBM <cburylb2@gmail.com>
2025-12-06 13:50:14 +0200

    added a footer in app.js

--- src/App.js
+++ src/App.js
@@ -2,5 +2,20 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      {" "}
+      Weather App
+      <footer>
+        This project was coded by Charllotte Blackwell-Maleshkov and is
+        <a
+          href="https://github.com/CharllieeBM/react-weather-directory"
+          target="_blank"
+          rel="noreferrer"
+        > Open-sourced on GitHub.
+        </a>
+        by
+      </footer>
+    </div>
+  );
 }
