10c46ba2559a6d68aa0b99e19e256d122981d1ea
CharllieeBM <cburylb2@gmail.com>
2025-12-06 22:59:17 +0200

    added a search button

--- src/Weather.js
+++ src/Weather.js
@@ -2,15 +2,26 @@ import React from "react";
 import "./Weather.css";
 
 export default function Weather() {
-  return  <div className="Weather">
-  <form>
-    <input type="search" placeholder="Enter a city.." className="form-control" />
-    <input type="submit" value="Search" className="btn btn-primary" />
-  </form>
-    <h1>Toronto</h1>
-    <ul>
-      <li>Monday 10:00</li>
-      <li>Sunny</li>
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>Toronto</h1>
+      <ul>
+        <li>Monday 10:00</li>
+        <li>Sunny</li>
       </ul>
       <div className="row">
         <div className="col-6">
@@ -31,5 +42,6 @@ export default function Weather() {
           </ul>
         </div>
       </div>
-  </div>;
+    </div>
+  );
 }
