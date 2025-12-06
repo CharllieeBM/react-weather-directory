0f8227dc007e224bf97caf976fcad67f6c1081d8
CharllieeBM <cburylb2@gmail.com>
2025-12-06 22:17:22 +0200

    added container in app.js for the bootstrap

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      {" "}
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Charllotte Blackwell-Maleshkov and is
-        open-sourced on{" "}
-        <a
-          href="https://github.com/CharllieeBM/react-weather-directory"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-        .
-      </footer>
+      <div className="container">
+        {" "}
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Charllotte Blackwell-Maleshkov and is
+          open-sourced on{" "}
+          <a
+            href="https://github.com/CharllieeBM/react-weather-directory"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
