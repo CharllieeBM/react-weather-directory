8439dbb72332a8ba0512e244b7fb8a1847bb6abc
CharllieeBM <cburylb2@gmail.com>
2025-12-06 13:52:16 +0200

    fixed word placings and links

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ export default function App() {
       Weather App
       <footer>
         This project was coded by Charllotte Blackwell-Maleshkov and is
+        open-sourced on{" "}
         <a
           href="https://github.com/CharllieeBM/react-weather-directory"
           target="_blank"
           rel="noreferrer"
-        > Open-sourced on GitHub.
+        >
+          GitHub
         </a>
-        by
+        .
       </footer>
     </div>
   );
