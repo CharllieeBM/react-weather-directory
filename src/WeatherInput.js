17e8712b8642d6872481d4a773c1f85b8e1e936a
CharllieeBM <cburylb2@gmail.com>
2025-12-10 02:20:26 +0200

    added component, changed the code, added code mapping, and fixed our css

--- src/WeatherInput.js
+++ src/WeatherInput.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInput(props) {
   return (
@@ -14,11 +15,9 @@ export default function WeatherInput(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix">
-            <img
-              src={props.data.iconUrl}
-              alt={props.data.description}
-              className="float-left"
-            />
+            <div className="float-left">
+            <WeatherIcon code={props.data.icon} />
+            </div>
             <span className="temperature">{props.data.temperature}</span>
             <span className="units">°C</span>
           </div>
